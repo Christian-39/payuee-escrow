a9678e379ebd98db64caadbd07047d64e2dd19e5
Christian-39 <ezechristian1999@gmail.com>
2026-05-11 00:34:50 +0100

    page

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -19,7 +19,7 @@ from .payuee_client import PayueeClient
 
 class StandardResultsSetPagination(PageNumberPagination):
     """Standard pagination class."""
-    page_size = 10
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
